20f6daaf11517f5a3315050344143af5e534f6e0
Raviteja02 <satyasairaviteja05@gmail.com>
2019-03-15 06:48:24 +0530

    Finalized Admin Module

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask,render_template,request, session, redirect, url_for,flash
 import os
 from pymongo import MongoClient as mc
+#from flask_pymongo import PyMongo
 import json
 import requests
 app=Flask(__name__)
 app.secret_key = os.urandom(24)
+
 
 connection = mc('localhost')
 database = connection.PharmacyDB2
@@ -13,6 +15,7 @@ collection2 = database.ReceivedStock
 collection3 = database.AvailableStock
 collection4 = database.PharmaReceivedStock
 collection5 = database.PharmaAvailableStock
+collection6 = database.StudentMedicineIssue
 
 
 @app.route("/")
@@ -23,9 +26,11 @@ def index():
 def AdminLogin():
 	return render_template("adminlogin.html")
 
+
 @app.route("/AdminPage")
 def AdminPage():
 	return render_template("MainAdminPage.html")
+
 
 
 @app.route("/StaffLogin")
@@ -34,7 +39,7 @@ def StaffLogin():
 
 @app.route("/StaffPage")
 def StaffPage():
-	return render_template("staffpage.html")
+	return render_template("accordian.html")
 
 @app.route("/StudentRegister", methods=['POST','GET'])
 def StudentRegister():
@@ -63,10 +68,12 @@ def StudentRegister():
 			addr  = request.form['address']
 			city  = request.form['city']
 			Accept = 'False'
+			profile_image=request.files['profile_image']
+			PyMongo.save_file(profile_image.filename, profile_image)
 			s_data={'Student_name' : sname,'Admission_number' : admno, 'Father_name' : fname, 'Mother_name' : mname, 'doj' : doj,
 			'coursecomplete' : doc, 'department' : dept, 'password' : pwd, 'gender' : sex, 'dob' : dob, 'relegion' : rel, 'caste' : caste,
 			'sub_caste' : s_caste, 'course' : course, 'contact_no' : phno, 'email' : email, 'aadhar_no' : adno, 'Age' : age,
-			'bloodgroup' :bgp, 'address' : addr, 'city' : city,'status':Accept}
+			'bloodgroup' :bgp, 'address' : addr, 'city' : city,'profile_image':profile_image.filename,'status':Accept}
 			collection.insert_one(s_data)
 			session['Admission_number'] = request.form['Admission_number']
 			print("data posted Successfully")
@@ -74,6 +81,7 @@ def StudentRegister():
 		return render_template("errors.html")
 	return render_template("studentreg2.html")
 
+	
 
 
 @app.route('/StudentLogin', methods=['POST','GET'])
@@ -107,35 +115,53 @@ def log():
 
 '''Data Posting'''
 
-@app.route('/MainStockEntry', methods=['POST','GET'])
+@app.route('/MainStockEntry',methods=['POST','GET'])
 def StockEntry():
 	if request.method=='POST':
 		InvoiceNumber = request.form['invoice_number']
 		InvoiceDate = request.form['invoice_date']
 		StockEntryDate = request.form['entry_date']
 		RecievedFrom = request.form['recieved_from']
-		TabletName = request.form['tab_name']
-		TotalTabsRecieved = int(request.form['total_tabs'])
-		RatePerUnit = int(request.form['rate/unit'])
-		TotalCost = int(request.form['total_bal'])
-		insert = {'InvoiceNumber' :InvoiceNumber, 'InvoiceDate':InvoiceDate, 'StockEntryDate':StockEntryDate, 'RecievedFrom':RecievedFrom,
-		'TabletName':TabletName,'TotalTabsRecieved':TotalTabsRecieved,'RatePerUnit':RatePerUnit,'TotalCost':TotalCost}
-		collection2.insert_one(insert)
-		print('data posted Successfully')
-		insert1 = {'TabletName':TabletName, 'TotalTabs':TotalTabsRecieved, 'RatePerUnit':RatePerUnit}
+		TotalCost = request.form['total_bal']
+		count = int(request.form['czContainer_czMore_txtCount'])
+		i = 1
+		while i<=count:
+			TabletName = request.form['tab_'+str(i)+'_name']
+			TotalTabsRecieved = int(request.form['tab_'+str(i)+'_qty'])
+			RatePerUnit = int(request.form['tab_'+str(i)+'_rate'])
+			insert = {'InvoiceNumber' :InvoiceNumber, 'InvoiceDate':InvoiceDate, 'StockEntryDate':StockEntryDate, 'RecievedFrom':RecievedFrom,
+			'TabletName':TabletName,'TotalTabsRecieved':TotalTabsRecieved,'RatePerUnit':RatePerUnit,'TotalCost':TotalCost}
+			collection2.insert_one(insert)
 
-		for x in collection3.find({'TabletName':TabletName}):
-			print(x['TabletName'])
-			if x['TabletName'] ==TabletName:
-				existingTabs=x['TotalTabs']
-				tabs = existingTabs+TotalTabsRecieved
-				collection3.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+			insert1 = {'TabletName':TabletName, 'TotalTabs':TotalTabsRecieved, 'RatePerUnit':RatePerUnit}
 
-		if collection3.count_documents({'TabletName':TabletName})==0:
-			print('hello')
-			collection3.insert_one(insert1)
-		
+			for x in collection3.find({'TabletName':TabletName}):
+				print(x['TabletName'])
+				if x['TabletName'] ==TabletName:
+					existingTabs=x['TotalTabs']
+					tabs = existingTabs+TotalTabsRecieved
+					collection3.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+
+			if collection3.count_documents({'TabletName':TabletName})==0:
+				collection3.insert_one(insert1)
+
+			i += 1
+
+
+		invoicedetails = collection2.find()
+		invoice=[]
+		for x in invoicedetails:
+			invoice.append({"InvoiceNumber":x['InvoiceNumber'],"InvoiceDate":x['InvoiceDate'],"StockEntryDate":x['StockEntryDate'],
+				"RecievedFrom":x['RecievedFrom'],"TabletName":x['TabletName'],"TotalTabsRecieved":x['TotalTabsRecieved'],
+				"RatePerUnit":x['RatePerUnit'],"TotalCost":x['TotalCost'] })
+		flash(invoice)
+
 		return render_template("invoices.html")
+		
+	return render_template("MainStockEntyForm.html")
+
+
+
 
 
 
@@ -145,44 +171,89 @@ def StockEntry():
 def pharmastockissue():
 	if request.method == 'POST':
 		StockIssueDate = request.form['issue_date']
-		TabletName = request.form['tab_name']
-		TotalTabsIssued = int(request.form['tabs_qty'])
-		RatePerUnit = int(request.form['rate/unit'])
 		TotalCost = int(request.form['total_bal'])
+		count = int(request.form['czContainer_czMore_txtCount'])
+		print(count)
+		i = 1
+		while i<=count:
+			TabletName = request.form['tab_'+str(i)+'_name']
+			TotalTabsIssued = int(request.form['tab_'+str(i)+'_qty'])
+			RatePerUnit = int(request.form['tab_'+str(i)+'_rate'])
+			insert = {'StockIssueDate':StockIssueDate, 'TabletName':TabletName, 'TotalTabsIssued':TotalTabsIssued,
+			'RatePerUnit':RatePerUnit, 'TotalCost':TotalCost}
 
-		insert = {'StockIssueDate':StockIssueDate, 'TabletName':TabletName, 'TotalTabsIssued':TotalTabsIssued,
-		'RatePerUnit':RatePerUnit, 'TotalCost':TotalCost}
+			for x in collection3.find({'TabletName':TabletName}):
+				print(x['TabletName'])
+				if x['TabletName'] ==TabletName:
+					existingTabs=x['TotalTabs']
+					if existingTabs>=TotalTabsIssued:
+						tabs = existingTabs-TotalTabsIssued
+						collection3.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+						collection4.insert_one(insert)
+						print('data posted Successfully')
+						insert1 = {'TabletName':TabletName, 'TotalTabs':TotalTabsIssued, 'RatePerUnit':RatePerUnit}
+					return 'Issued Quantiy Of ' +TabletName+ ' Are Not Available in Stock.:' +str(existingTabs)
 
-		for x in collection3.find({'TabletName':TabletName}):
-			print(x['TabletName'])
-			if x['TabletName'] ==TabletName:
-				existingTabs=x['TotalTabs']
-				tabs = existingTabs-TotalTabsIssued
-				collection3.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+			for x in collection5.find({'TabletName':TabletName}):
+				print(x['TabletName'])
+				if x['TabletName'] ==TabletName:
+					existingTabs=x['TotalTabs']
+					tabs = existingTabs+TotalTabsIssued
+					collection5.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
 
-		collection4.insert_one(insert)
-		print('data posted Successfully')
-		insert1 = {'TabletName':TabletName, 'TotalTabs':TotalTabsIssued, 'RatePerUnit':RatePerUnit}
+			if collection5.count_documents({'TabletName':TabletName})==0:
+				print('hello')
+				collection5.insert_one(insert1)
+			i += 1
 
-		for x in collection5.find({'TabletName':TabletName}):
-			print(x['TabletName'])
-			if x['TabletName'] ==TabletName:
-				existingTabs=x['TotalTabs']
-				tabs = existingTabs+TotalTabsIssued
-				collection5.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+		pharmaissues = collection4.find()
+		issues = []
+		for x in pharmaissues:
+			issues.append({"StockIssueDate":x['StockIssueDate'],"TabletName":x['TabletName'],"TotalTabsIssued":x['TotalTabsIssued'],
+				"RatePerUnit":x['RatePerUnit'],"TotalCost":x['TotalCost']})
+		flash(issues)
+		return render_template('Pharmaissues.html')
+		
+	return render_template("PharmacyStockIssue.html")
 
-		if collection5.count_documents({'TabletName':TabletName})==0:
-			print('hello')
-			collection5.insert_one(insert1)
+@app.route('/StudentMedicineIssue',methods=['POST','GET'])
+def MedicineIssue():
+	if request.method=='POST':
+		Admission_number=request.form['Admission_number']
+		Op_number=request.form['Op_number']
+		issue_date=request.form['issue_date']
+		Totalbal = request.form['total_bal']
+		count = int(request.form['czContainer_czMore_txtCount'])
+		print(count)
+		i = 1
+		while i<=count:
+			TabletName = request.form['tab_'+str(i)+'_name']
+			TotalTabsIssued = int(request.form['tab_'+str(i)+'_qty'])
+			RatePerUnit = int(request.form['tab_'+str(i)+'_rate'])
+			insert = {'Admission_number':Admission_number, 'Op_number':Op_number,'issue_date':issue_date,'tab_name':TabletName,
+			'qty_issued':TotalTabsIssued,'RatePerunit':RatePerUnit,'TotalBill':Totalbal}
 
-		return render_template("StockIssued.html")
+			for x in collection5.find({'TabletName':TabletName}):
+				print(x['TabletName'])
+				if x['TabletName'] ==TabletName:
+					existingTabs=x['TotalTabs']
+					if existingTabs>=TotalTabsIssued:
+						tabs = existingTabs-TotalTabsIssued
+						collection5.update_one({'TabletName':TabletName},{'$set': {'TotalTabs':tabs}})
+						print('data updated successfully')
+					return 'Issued Quantiy Of ' +TabletName+ ' Are Not Available in Stock.:' +str(existingTabs)
 
-
-
-
+			collection6.insert_one(insert)
+			i += 1
+		print('medicine issued successfully')
+		return 'medicine issued successfully!'
+	return render_template("StudentMedicineIssue.html")
 
 
 '''Data Posting Ending'''
+
+
+
 
 
 '''Data Retreiving'''
@@ -234,6 +305,16 @@ def viewpharmastoc():
 		flash(ph_stock)
 		return render_template('PharmaStock.html')
 
+@app.route('/ViewLowStock',methods=['POST','GET'])
+def viewlowstock():
+	if request.method=='GET':
+		lowstock = collection3.find({'TotalTabs':{'$lt' : 50 }})
+		low_stock=[]
+		for x in lowstock:
+			low_stock.append({"TabletName":x['TabletName'],"TotalTabs":x['TotalTabs'],"RatePerUnit":x['RatePerUnit']})
+		flash(low_stock)
+		return render_template('LowStock.html')
+
 @app.route('/ViewStudentDetails',methods=['POST','GET'])
 def VeiwDetails():
 	if request.method=='POST':
@@ -259,8 +340,11 @@ def ViewStudentRegistrations():
 
 
 
-
 '''Dtat Retreiving Ending'''
+
+
+
+
 
 @app.route('/apiconnect',methods=['POST','GET'])
 def apicon():
